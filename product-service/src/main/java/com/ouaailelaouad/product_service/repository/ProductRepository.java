ec01517455cf60b0880a210de5237027b022bf4f
OuaailElaouad <2003waelelaouad@gmail.com>
2025-02-24 14:54:10 +0000

    finishing controller, DTOs and product service

---
+++ product-service/src/main/java/com/ouaailelaouad/product_service/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.ouaailelaouad.product_service.repository;
+
+
+import com.ouaailelaouad.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
