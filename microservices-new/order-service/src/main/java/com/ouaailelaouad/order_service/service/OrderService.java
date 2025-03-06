535d9d8a649310037e642d253c25d559aacace54
OuaailElaouad <2003waelelaouad@gmail.com>
2025-03-06 03:06:20 +0000

    LoadBalancing

--- microservices-new/order-service/src/main/java/com/ouaailelaouad/order_service/service/OrderService.java
+++ microservices-new/order-service/src/main/java/com/ouaailelaouad/order_service/service/OrderService.java
@@ -27,7 +27,7 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     private ObservationRegistry observationRegistry;
     private final ApplicationEventPublisher eventPublisher;
 
@@ -47,8 +47,8 @@ public class OrderService {
                 .toList();
 
         // Call Inventory Service to check stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
